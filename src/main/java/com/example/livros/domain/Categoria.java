82986ad6f1d0fea3ff6cf10e727b82a92337b552
ScotLion <alessandrodasilva93@gmail.com>
2021-11-13 13:35:31 -0300

    Finalizando as classes de Dominio

--- src/main/java/com/example/livros/domain/Categoria.java
+++ src/main/java/com/example/livros/domain/Categoria.java
@@ -3,16 +3,42 @@
 //ctrl + shift + F organiza o codigo
 package com.example.livros.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+// @Entity vai cria uma tabela na base de dados
+//@Entity name = "tbl_categoria"; é possivel informa o nome da tabela se nao informa Entity cria a tabela com o nome da classe
+
+@Entity
+public class Categoria implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// Essa notação informa que id é uma chave primaria
+	//GeneratedValue(strategy = GenerationType.IDENTITY)
+	// IDENTITY é usado pelo MySQL (AUTO_INCREMENT), Oracle 12c, DB2...
+	// SEQUENCE é usado pelo PostGres, DB2 ...
+	//@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Integer id;
 	private String nome;
 	private String Descricao;
 
+	//@OneToMany é usado para indicar relação de 1 para N uma categoria possui varius livros
+	//mappedBy é usado para mapear
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> listaLivro = new ArrayList<>();
 
 	public Categoria() {
--- src/main/java/com/example/livros/domain/Livro.java
+++ src/main/java/com/example/livros/domain/Livro.java
@@ -1,13 +1,34 @@
 package com.example.livros.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.ManyToAny;
+
+@Entity
+public class Livro implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Integer id;
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
-
+	
+	//@ManyToAny n para 1 relação de muito para 1 - muitos livros em uma categoria
+	@ManyToAny(mappedBy = "categoria")
 	private Categoria categoria;
 
 	public Livro() {
