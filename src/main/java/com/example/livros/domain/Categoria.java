618f5a289eaea9cdad954193791ed9e4720b866c
ScotLion <alessandrodasilva93@gmail.com>
2021-11-13 13:07:05 -0300

    comite inicial 2

---
+++ src/main/java/com/example/livros/domain/Categoria.java
@@ -0,0 +1,78 @@
+//alt + shift + s menu
+//ctrl + d deleta uma linha
+//ctrl + shift + F organiza o codigo
+package com.example.livros.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Categoria {
+
+	private Integer id;
+	private String nome;
+	private String Descricao;
+
+	private List<Livro> listaLivro = new ArrayList<>();
+
+	public Categoria() {
+		super();
+	}
+
+	public Categoria(Integer id, String nome, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		Descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return Descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+
+	public List<Livro> getListaLivro() {
+		return listaLivro;
+	}
+
+	public void setListaLivro(List<Livro> listaLivro) {
+		this.listaLivro = listaLivro;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
